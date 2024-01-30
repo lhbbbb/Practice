dc8a899200889cca7776055fca7785f2082314b5
hyunbin <lhbbbb@github.com>
2024-01-30 23:35:33 +0900

    jpa 값 타입 컬렉션

--- spring/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ spring/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -18,8 +18,19 @@ public class JpaMain {
             member.setName("john");
             Period period = new Period(LocalDateTime.now(), LocalDateTime.now().plusHours(1));
             member.setPeriod(period);
-            member.setAddress(new Address());
+            member.setAddress(new Address("Seoul", "ro", "10000"));
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
             em.persist(member);
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
--- spring/ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ spring/ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -1,9 +1,11 @@
 package hellojpa;
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member extends BaseEntity {
@@ -14,6 +16,26 @@ public class Member extends BaseEntity {
     private Period period;
     @Embedded
     private Address address;
+
+    // 값 타입 컬렉션이 단순한 경우에는 사용 가능.
+    @ElementCollection
+    @CollectionTable(name="FAVORITE_FOOD", joinColumns = @JoinColumn(name="id"))
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    /* 값 타입 컬렉션은 기본적으로 지연로딩. 실무에선 값 타입 컬렉션을 잘 사용 X. 주인과 관련된 데이터를 모두 삭제하고 다시 insert 하는 등 관련해서 복잡한 case 들이 등장할 수 있음
+       1:N 매핑을 통해서 해결하는 방향이 더 좋음
+     */
+    @ElementCollection
+    @CollectionTable(name="ADDRESSHISTORY", joinColumns = @JoinColumn(name="id"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
