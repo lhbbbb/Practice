78f8bffddab1be811429b35ab1d17034431ab663
hyunbin <lhbbbb@github.com>
2024-02-20 23:44:56 +0900

    add querydsl bulk

--- spring/ex3-jpa-querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ spring/ex3-jpa-querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
 import study.querydsl.dto.QMemberDto;
@@ -376,5 +377,30 @@ public class QuerydslBasicTest {
 
     private BooleanExpression allEq(String usernameCond, Integer ageCond) {
         return usernameEq(usernameCond).and(ageEq(ageCond));
+    }
+
+    @Test
+    @Commit
+    public void bulkUpdate() {
+        //member1 -> 비회원
+        //member2 -> 비회원
+
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))
+                .execute();
+
+        //bulk 연산 후 영속성 컨텍스트 초기화 필수!
+        em.flush();
+        em.clear();
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
